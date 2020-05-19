5c0ed45a10e337e2713545a8e4fad6c240ad9b50
BingNa Yang <bingnayang@gmail.com>
2020-05-18 22:59:01 -0400

    Initial commit

---
+++ src/com/company/Lottery.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Lottery {
+    private ArrayList<Integer> luckyPickNumbers;
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        gameIntro();
+    }
+    public static void gameIntro(){
+        System.out.println("Welcome to Lucky Pick Lottery!!!");
+        System.out.println("Number range are from 1 to 100");
+        System.out.println("Pick 4 base number and one lucky number");
+    }
+
+}
