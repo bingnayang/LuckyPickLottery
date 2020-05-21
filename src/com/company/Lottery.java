a70c0b305ef6b5631b78258f36860c4e7e661858
BingNa Yang <bingnayang@gmail.com>
2020-05-20 20:20:10 -0400

    update lottery methods
    update instant variables
    prevent user input duplicate base number
    sent back base number match count

--- src/com/company/Lottery.java
+++ src/com/company/Lottery.java
@@ -1,8 +1,31 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Lottery {
-    private ArrayList<Integer> luckyPickNumbers;
+    // Random generate four base numbers
+    private ArrayList<Integer> baseNumbersGenerated = new ArrayList<>(Arrays.asList(3,67,23,12));
+    // Random generated a lucky number
+    private int luckyNumberGenerated = 56;
+    private int luckyNumberHit = 0;
+    private int baseNumberHit = 0;
+    private int hitsPoints = 0;
+
+
+    public int checkBaseNumber(ArrayList basePickNumbers){
+        for(int i=0; i<basePickNumbers.size(); i++){
+            if(baseNumbersGenerated.contains(basePickNumbers.get(i))){
+                baseNumberHit++;
+            }
+        }
+        return baseNumberHit;
+    }
+    public boolean checkLuckyNumber(int pickedLuckyNumber){
+        if(pickedLuckyNumber == luckyNumberGenerated){
+            return true;
+        }
+        return false;
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,66 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> basePick = new ArrayList<>();
+        Lottery lottery = new Lottery();
+        int numberInput;
+        int baseNumber = 1;
         gameIntro();
+        gameRewards();
+
+        // Ask to input the four base numbers and the lucky number.
+        while (true){
+            System.out.println("Enter the lucky *base["+baseNumber+"]* number: ");
+            numberInput = scanner.nextInt();
+            if(basePick.contains(numberInput)){
+                System.out.println("It a duplicate number, enter another new *base* number :");
+            }else{
+                basePick.add(numberInput);
+                baseNumber++;
+            }
+            if(basePick.size() == 4){
+                break;
+            }
+        }
+        System.out.println("Enter the lucky *lucky* number: ");
+        int luckyPick = scanner.nextInt();
+
+
+        // Print out player lucky picked numbers
+        System.out.println("Base number pick: "+Arrays.toString(basePick.toArray())+" Lucky number pick: "+luckyPick);
+
+
+        // Checking how many base numbers match
+        int baseNumberMatch = lottery.checkBaseNumber(basePick);
+        System.out.println("Base Number Match Hits: "+baseNumberMatch);
+        // Checking if the lottery number match
+        boolean luckyNumberMatch = lottery.checkLuckyNumber(luckyPick);
+        System.out.println("Lucky Number Match: "+luckyNumberMatch);
+
     }
     public static void gameIntro(){
-        System.out.println("Welcome to Lucky Pick Lottery!!!");
-        System.out.println("Number range are from 1 to 100");
-        System.out.println("Pick 4 base number and one lucky number");
+        System.out.println("=====================================================================");
+        System.out.println("= Welcome to Lucky Pick Lottery!!!");
+        System.out.println("= Number range are from 1 to 100");
+        System.out.println("= Pick 4 base number and one lucky number");
+        System.out.println("= Four Base Number Cannot Be Same");
+        System.out.println("=====================================================================");
+
+    }
+    public static void gameRewards(){
+        System.out.println("=====================================================================");
+        System.out.println("= Match All: \t\t\t\t\t\t\t\t 100,000 Lucky Coins");
+        System.out.println("= Match Lucky Number and Two Base Numbers: \t 50,000 Lucky Coins");
+        System.out.println("= Match All Base Numbers : \t\t\t\t\t 1,000 Lucky Coins ");
+        System.out.println("= Match Only Lucky Number: \t\t\t\t\t 500 Lucky Coins ");
+        System.out.println("=====================================================================");
     }
 
 }
